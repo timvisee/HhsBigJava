9781234f4a5b03150a4ca165e3deac0903149be1
timvisee <timvisee@gmail.com>
2016-02-08 16:51:02 +0100

    Finished challenge P3.32

--- BigJavaP3.32/src/com/timvisee/hhs/Main.java
+++ BigJavaP3.32/src/com/timvisee/hhs/Main.java
@@ -15,10 +15,17 @@
 
 package com.timvisee.hhs;
 
+import java.util.Scanner;
+
 /**
  * Main class.
  */
 public class Main {
+
+    /**
+     * Pin number.
+     */
+    public static final int PIN = 1234;
 
     /**
      * Main method, called on start.
@@ -26,6 +33,35 @@ public class Main {
      * @param args Start arguments.
      */
     public static void main(String[] args) {
+        // Create a scanner for user input
+        Scanner in = new Scanner(System.in);
 
+        // Define some variables
+        int tries = 0;
+
+        // Start a while loop and ask for the pin
+        while(true) {
+            // Ask the user for a PIN number
+            System.out.println("Enter your PIN number:");
+            int pin = in.nextInt();
+
+            // Compare the PIN
+            if(pin == PIN) {
+                System.out.println("Your PIN is correct!");
+                System.exit(0);
+            }
+
+            // Show an error message
+            System.out.println("Your PIN is incorrect!");
+
+            // Increase the tries number
+            tries++;
+
+            // If the user has tried to much, show an error
+            if(tries >= 3) {
+                System.out.println("Your bank card is blocked!");
+                System.exit(0);
+            }
+        }
     }
 }
