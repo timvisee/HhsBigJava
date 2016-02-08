4e2c15b5ed02cc6e00c452a67d58e6e0a5b91e06
timvisee <timvisee@gmail.com>
2016-02-08 15:29:21 +0100

    Added challenge docs

--- BigJavaP3.5/src/com/timvisee/hhs/Main.java
+++ BigJavaP3.5/src/com/timvisee/hhs/Main.java
@@ -1,3 +1,11 @@
+/**
+ * BigJava P3.5 challenge:
+ * Write a program that reads three numbers and prints “increasing” if they are in increasing order, “decreasing” if they are in decreasing order, and “neither” otherwise. Here, “increasing” means “strictly increasing”, with each value larger than its pre decessor. The sequence 3 4 4 would not be considered increasing.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
 package com.timvisee.hhs;
 
 /**
