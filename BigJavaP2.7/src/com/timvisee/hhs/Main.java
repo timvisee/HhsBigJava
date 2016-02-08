abe281d5cd3ca18509a7a1df1aa75c9e7bffd99c
timvisee <timvisee@gmail.com>
2016-02-08 16:22:22 +0100

    Finished P2.7 challenge

---
+++ BigJavaP2.7/src/com/timvisee/hhs/Main.java
@@ -0,0 +1,42 @@
+/**
+ * BigJava P2.7 challenge:
+ * Write a program that prompts the user for a radius and then prints:
+ * A. The area and circumference of a circle with that radius.
+ * B. The volume and surface area of a sphere with that radius.
+ */
+
+package com.timvisee.hhs;
+
+import java.util.Scanner;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+        // Create a scanner instance
+        Scanner in = new Scanner(System.in);
+
+        // Ask the user for a radius value
+        double radius = in.nextDouble();
+
+        // Calculate the area, circumference and volume
+        double area = Math.PI * Math.pow(radius, 2);
+        double circumference = Math.PI  * 2 * radius;
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+        double surface = 4.0 * Math.PI * Math.pow(radius, 2);
+
+        // Print the results
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + area);
+        System.out.println("Circumference: " + circumference);
+        System.out.println("Volume: " + volume);
+        System.out.println("Surface: " + surface);
+    }
+}
