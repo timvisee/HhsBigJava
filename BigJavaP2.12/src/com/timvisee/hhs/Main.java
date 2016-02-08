4c02976d0dd3aab626a06ea725e8627312150a04
timvisee <timvisee@gmail.com>
2016-02-08 16:27:11 +0100

    Created P2.12 challenge

---
+++ BigJavaP2.12/src/com/timvisee/hhs/Main.java
@@ -0,0 +1,24 @@
+/**
+ * BigJava P2.12 challenge:
+ * File names and extensions. Write a program that prompts the user for the drive letter (C),
+ * the path (\Windows\System), the file name (Readme), and the extension (txt). Then print the complete file name
+ * C:\Windows\System\Readme.txt. (If you use UNIX or a Macintosh, skip the drive name and use / instead of \ to separate
+ * directories.)
+ */
+
+package com.timvisee.hhs;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
