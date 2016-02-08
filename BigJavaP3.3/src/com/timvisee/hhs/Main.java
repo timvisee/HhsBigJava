f69114312c168e949afb00d259e820710513f54e
timvisee <timvisee@gmail.com>
2016-02-08 16:33:23 +0100

    Created P3.3 challenge

---
+++ BigJavaP3.3/src/com/timvisee/hhs/Main.java
@@ -0,0 +1,23 @@
+/**
+ * BigJava P3.3 challenge:
+ * Write a program that reads an integer and prints how many digits the number has, by
+ * checking whether the number is ≥ 10, ≥ 100, and so on. (Assume that all integers are
+ * less than ten billion.) If the number is negative, first multiply it with –1.
+ */
+
+package com.timvisee.hhs;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
