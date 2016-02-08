f9358e55882c31644874a4687657e3f129ba1c88
timvisee <timvisee@gmail.com>
2016-02-08 15:45:08 +0100

    Formatted docs, created base

--- BigJavaP4.1/src/com/timvisee/hhs/Main.java
+++ BigJavaP4.1/src/com/timvisee/hhs/Main.java
@@ -1,7 +1,26 @@
+/**
+ * BigJava P4.1 challenge:
+ * Write programs with loops that compute
+ * a. The sum of all even numbers between 2 and 100 (inclusive).
+ * b. The sum of all squares between 1 and 100 (inclusive).
+ * c. All powers of 2 from 20 up to 220.
+ * d. The sum of all odd numbers between a and b (inclusive), where a and b are inputs.
+ * e. The sum of all odd digits of an input. (For example, if the input is 32677, the sum would be 3 + 7 + 7 = 17.)
+ */
+
 package com.timvisee.hhs;
 
 /**
- * Created by Tim on 8-2-2016.
+ * Main class.
  */
 public class Main {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
 }
