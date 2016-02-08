139c6e4c7d560becb3d8f1141abd4230dbaf2322
timvisee <timvisee@gmail.com>
2016-02-08 16:35:03 +0100

    Finished challenge P3.3

--- BigJavaP3.3/src/com/timvisee/hhs/Main.java
+++ BigJavaP3.3/src/com/timvisee/hhs/Main.java
@@ -7,6 +7,8 @@
 
 package com.timvisee.hhs;
 
+import java.util.Scanner;
+
 /**
  * Main class.
  */
@@ -18,6 +20,23 @@ public class Main {
      * @param args Start arguments.
      */
     public static void main(String[] args) {
-        
+        // Create a scanner for user input
+        Scanner in = new Scanner(System.in);
+
+        // Ask for an input value
+        System.out.println("Enter an input value:");
+        int val = in.nextInt();
+
+        // Absolute the value
+        val = Math.abs(val);
+
+        // Convert the number to a string
+        String valStr = String.valueOf(val);
+
+        // Count the number of digits the number has
+        int digitCount = valStr.length();
+
+        // Print the results
+        System.out.println("Number of digits: " + digitCount);
     }
 }
