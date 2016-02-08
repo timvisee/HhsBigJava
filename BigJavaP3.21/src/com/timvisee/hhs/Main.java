eae874bc71e15a7a07f3241f01df50e534d28b4c
timvisee <timvisee@gmail.com>
2016-02-08 16:38:11 +0100

    Created P3.21 challenge

---
+++ BigJavaP3.21/src/com/timvisee/hhs/Main.java
@@ -0,0 +1,31 @@
+/**
+ * BigJava P3.21 challenge:
+ * The original U.S. income tax of 1913 was quite simple. The tax was:
+ * - 1 percent on the first $50,000.
+ * - 2 percent on the amount over $50,000 up to $75,000.
+ * - 3 percent on the amount over $75,000 up to $100,000.
+ * - 4 percent on the amount over $100,000 up to $250,000.
+ * - 5 percent on the amount over $250,000 up to $500,000.
+ * - 6 percent on the amount over $500,000.
+ * There was no separate schedule for single or married taxpayers. Write a program that computes the income tax according to this schedule.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhs;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
