d6c35b239862fdcb4ae118c546165841f1d5a49d
timvisee <timvisee@gmail.com>
2016-02-08 15:35:03 +0100

    Reformatted code, with right margin for easier reading

--- BigJavaP3.4/src/com/timvisee/hhs/Main.java
+++ BigJavaP3.4/src/com/timvisee/hhs/Main.java
@@ -1,6 +1,6 @@
 /**
- * BigJava P3.4 challenge:
- * Write a program that reads three numbers and prints “all the same” if they are all the same, “all different” if they are all different, and “neither” otherwise.
+ * BigJava P3.4 challenge: Write a program that reads three numbers and prints “all the same” if they are all the same,
+ * “all different” if they are all different, and “neither” otherwise.
  *
  * @author Tim Visée
  * @website www.timvisee.com
