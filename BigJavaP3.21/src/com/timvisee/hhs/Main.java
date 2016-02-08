6b972d88860e51add202e9a1873d7832f16d7c70
timvisee <timvisee@gmail.com>
2016-02-08 16:42:15 +0100

    Finished challenge P3.21

--- BigJavaP3.21/src/com/timvisee/hhs/Main.java
+++ BigJavaP3.21/src/com/timvisee/hhs/Main.java
@@ -7,13 +7,16 @@
  * - 4 percent on the amount over $100,000 up to $250,000.
  * - 5 percent on the amount over $250,000 up to $500,000.
  * - 6 percent on the amount over $500,000.
- * There was no separate schedule for single or married taxpayers. Write a program that computes the income tax according to this schedule.
+ * There was no separate schedule for single or married taxpayers. Write a program that computes the income tax
+ * according to this schedule.
  *
  * @author Tim Visee
  * @website www.timvisee.com
  */
 
 package com.timvisee.hhs;
+
+import java.util.Scanner;
 
 /**
  * Main class.
@@ -26,6 +29,30 @@ public class Main {
      * @param args Start arguments.
      */
     public static void main(String[] args) {
+        // Create a scanner instance for user input
+        Scanner in = new Scanner(System.in);
 
+        // Ask the user for a price
+        System.out.println("Enter a price:");
+        double price = in.nextDouble();
+
+        // Calculate the tax
+        if(price <= 50000)
+            System.out.println("Income tax: " + (price * 0.01));
+
+        else if(price <= 75000)
+            System.out.println("Income tax: " + (price * 0.02));
+
+        else if(price <= 100000)
+            System.out.println("Income tax: " + (price * 0.03));
+
+        else if(price <= 250000)
+            System.out.println("Income tax: " + (price * 0.04));
+
+        else if(price <= 500000)
+            System.out.println("Income tax: " + (price * 0.05));
+
+        else
+            System.out.println("Income tax: " + (price * 0.06));
     }
 }
