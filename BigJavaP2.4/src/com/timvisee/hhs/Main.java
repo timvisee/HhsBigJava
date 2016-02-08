e321fba2827f07fb2bc7ae307d984b7adb1ae7a8
timvisee <timvisee@gmail.com>
2016-02-08 16:13:15 +0100

    Created challenge P2.4

--- BigJavaP2.4/src/com/timvisee/hhs/Main.java
+++ BigJavaP2.4/src/com/timvisee/hhs/Main.java
@@ -1,7 +1,23 @@
+/**
+ * BigJava P2.4 challenge:
+ * Write a program that prompts the user for two integers and then prints
+ * A. The sum.
+ * B. The difference.
+ */
+
 package com.timvisee.hhs;
 
 /**
- * Created by Tim on 8-2-2016.
+ * Main class.
  */
 public class Main {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
 }
