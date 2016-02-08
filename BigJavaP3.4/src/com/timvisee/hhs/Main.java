673460b1358431a8ecb9d70ec012316d51c973b2
timvisee <timvisee@gmail.com>
2016-02-08 15:24:55 +0100

    Completed first challenge

--- BigJavaP3.4/src/com/timvisee/hhs/Main.java
+++ BigJavaP3.4/src/com/timvisee/hhs/Main.java
@@ -1,7 +1,29 @@
 package com.timvisee.hhs;
 
-/**
- * Created by Tim on 8-2-2016.
- */
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        // Create a scanner instance, and an array for the three numbers
+        Scanner in = new Scanner(System.in);
+        int[] nums = new int[3];
+
+        // Ask the user for three number
+        System.out.println("Please enter three numbers:");
+
+        // Gather the input
+        for(int i = 0; i < 3; i++)
+            nums[i] = in.nextInt();
+
+        // Compare the numbers
+        if(nums[0] == nums[1] && nums[1] == nums[2])
+            System.out.println("all the same");
+
+        else if(nums[0] != nums[1] && nums[0] != nums[2] && nums[1] != nums[2])
+            System.out.println("all different");
+
+        else
+            System.out.println("neither");
+    }
 }
