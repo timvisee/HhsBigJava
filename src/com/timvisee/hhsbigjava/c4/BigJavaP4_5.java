c878d7785e0351e4f5708dd70af5c4bb9c4bfdf0
timvisee <timvisee@gmail.com>
2016-02-10 12:24:13 +0100

    Completed the BigJava P4.5 challenge

--- src/com/timvisee/hhsbigjava/c4/BigJavaP4_5.java
+++ src/com/timvisee/hhsbigjava/c4/BigJavaP4_5.java
@@ -14,6 +14,10 @@
 
 package com.timvisee.hhsbigjava.c4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Main class.
  */
@@ -25,6 +29,65 @@ public class BigJavaP4_5 {
      * @param args Start arguments.
      */
     public static void main(String[] args) {
-        //
+        // Create a scanner for user input
+        Scanner in = new Scanner(System.in);
+
+        // Ask the user for input values
+        System.out.println("Please enter some floating point values, end with an empty line:");
+
+        // Create a list for the numbers
+        List<Float> floats = new ArrayList<>();
+
+        // Gather the input
+        while(true) {
+            // Gather the input
+            String line = in.nextLine();
+
+            // If the line is blank, continue
+            if(line.trim().length() == 0)
+                break;
+
+            // Try to parse the line as a float value
+            try {
+                floats.add(Float.valueOf(line));
+
+            } catch(Exception ex) {
+                System.out.println("The value you've entered is invalid, please enter a new valid value.");
+            }
+        }
+
+        // Make sure some values are entered
+        if(floats.size() == 0) {
+            System.out.println("You haven't entered any floating point values, the program will quit!");
+            System.exit(0);
+        }
+
+        // Loop through the values and determine the minimum, maximum and the sum
+        float min = floats.get(0);
+        float max = floats.get(0);
+        float total = 0.0f;
+        for(float f : floats) {
+            // Update the minimum value
+            if(f < min)
+                min = f;
+
+            // Update the maximum value
+            if(f > max)
+                max = f;
+
+            // Add the number to the total
+            total += f;
+        }
+
+        // Calculate the average and the range of the values
+        float avg = total / ((float) floats.size());
+        float range = Math.abs(max - min);
+
+        // Print the results
+        System.out.println("Number of values: " + floats.size());
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
+        System.out.println("Average of the values: " + avg);
+        System.out.println("Range of the values: " + range);
     }
 }
