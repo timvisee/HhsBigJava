39e93edc0cd3aadb29df54857ef725da3205bc5b
timvisee <timvisee@gmail.com>
2016-02-10 11:21:49 +0100

    Created BigJava P4.3 challenge base

---
+++ src/com/timvisee/hhsbigjava/c4/BigJavaP4_3.java
@@ -0,0 +1,29 @@
+/**
+ * BigJava P4.3 challenge:
+ * Write programs that read a line of input as a string and print:
+ * a. Only the uppercase letters in the string.
+ * b. Every second letter of the string.
+ * c. The string, with all vowels replaced by an underscore.
+ * d. The number of vowels in the string.
+ * e. The positions of all vowels in the string.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c4;
+
+/**
+ * Main class.
+ */
+public class BigJavaP4_3 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
