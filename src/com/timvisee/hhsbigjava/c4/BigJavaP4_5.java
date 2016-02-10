b4e4a9582645e5fd63265b997663e11ea93d318e
timvisee <timvisee@gmail.com>
2016-02-10 12:15:08 +0100

    Created the base of the BigJava P4.5 challenge

---
+++ src/com/timvisee/hhsbigjava/c4/BigJavaP4_5.java
@@ -0,0 +1,30 @@
+/**
+ * BigJava P4.5 challenge:
+ * Write a program that reads a set of floating­point values. Ask the user to enter the
+ * values, then print:
+ * - the average of the values.
+ * - the smallest of the values.
+ * - the largest of the values.
+ * - the range, that is the difference between the smallest and largest.
+ * Of course, you may only prompt for the values once.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c4;
+
+/**
+ * Main class.
+ */
+public class BigJavaP4_5 {
+
+    /**
+     * Main method, called ons start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+        //
+    }
+}
