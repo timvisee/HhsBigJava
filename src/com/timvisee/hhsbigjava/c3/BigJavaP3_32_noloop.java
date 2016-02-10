508089a89ec5eca34c52b789f87fae91782eaed1
timvisee <timvisee@gmail.com>
2016-02-10 10:51:20 +0100

    Created BigJava P3.32 challenge without the use of a loop

---
+++ src/com/timvisee/hhsbigjava/c3/BigJavaP3_32_noloop.java
@@ -0,0 +1,77 @@
+/**
+ * BigJava P3.32 challenge:
+ * When you use an automated teller machine (ATM) with your bank card, you need to use a personal identification number
+ * (PIN) to access your account. If a user fails more than three times when entering the PIN, the machine will block the
+ * card. Assume that the user’s PIN is “1234” and write a program that asks the user for the PIN no more than three times,
+ * and does the following:
+ * - If the user enters the right number, print a message saying, “Your PIN is correct”, and end the program.
+ * - If the user enters a wrong number, print a message saying, “Your PIN is incorrect” and, if you have asked for the PIN
+ *   less than three times, ask for it again.
+ * - If the user enters a wrong number three times, print a message saying “Your bank card is blocked” and end the program.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c3;
+
+import java.util.Scanner;
+
+/**
+ * Main class.
+ */
+public class BigJavaP3_32_NoLoop {
+
+    /**
+     * Pin number.
+     */
+    public static final int PIN = 1234;
+
+    /**
+     * Define a variable that holds the number of tries.
+     */
+    private static int tries = 0;
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+        // Check the pin recursively
+        checkPin();
+    }
+
+    /**
+     * Check the pin number, recursively.
+     */
+    public static void checkPin() {
+        // Create a scanner for user input
+        Scanner in = new Scanner(System.in);
+
+        // Ask the user for a PIN number
+        System.out.println("Enter your PIN number:");
+        int pin = in.nextInt();
+
+        // Compare the PIN
+        if(pin == PIN) {
+            System.out.println("Your PIN is correct!");
+            return;
+        }
+
+        // Show an error message
+        System.out.println("Your PIN is incorrect!");
+
+        // Increase the tries number
+        tries++;
+
+        // If the user has tried to much, show an error
+        if(tries >= 3) {
+            System.out.println("Your bank card is blocked!");
+            return;
+        }
+
+        // Call the check function again, recursively
+        checkPin();
+    }
+}
