56749ff3494152f891a90dcd66a4dd12cbc06864
timvisee <timvisee@gmail.com>
2016-02-10 10:56:16 +0100

    Created basis of BigJava P3.34 challenge

---
+++ src/com/timvisee/hhsbigjava/c3/BigJavaP3_34.java
@@ -0,0 +1,34 @@
+/**
+ * BigJava P3.34 challenge:
+ * A supermarket awards coupons depending on how much a customer spends on
+ * groceries. For example, if you spend $50, you will get a coupon worth eight percent
+ * of that amount. The following table shows the percent used to calculate the coupon
+ * awarded for different amounts spent. Write a program that calculates and prints the
+ * value of the coupon a person can receive based on groceries purchased.
+ *
+ * - Up to 10 dollars:          No coupon
+ * - From 10 to 60 dollars:     8% coupon
+ * - From 50 to 150 dollars:    10% coupon
+ * - From 150 to 210 dollars:   12% coupon
+ * - More than 210 dollars:     14% coupon
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c3;
+
+/**
+ * Main class.
+ */
+public class BigJavaP3_34 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
