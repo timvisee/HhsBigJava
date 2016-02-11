1ca544147467f1a47c06ec943ef4b10c7e179af8
timvisee <timvisee@gmail.com>
2016-02-11 11:15:08 +0100

    Created the BigJava P5.7 challenge class

---
+++ src/com/timvisee/hhsbigjava/c5/BigJavaP5_7.java
@@ -0,0 +1,25 @@
+/**
+ * BigJava P5.7 challenge:
+ * Write a method public static int countWords(String str) that returns a count of all words in the string str.
+ * Words are separated by spaces. For example, count Words("Mary had a little lamb") should return 5.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c5;
+
+/**
+ * Main class.
+ */
+public class BigJavaP5_7 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
