6fac5d32ec423ddc515b9f907c2f43d4b1ff10f9
timvisee <timvisee@gmail.com>
2016-02-11 11:27:21 +0100

    Created the BigJava P5.16 challenge class

---
+++ src/com/timvisee/hhsbigjava/c5/BigJavaP5_16.java
@@ -0,0 +1,19 @@
+/**
+ * BigJava P5.16 challenge:
+ * Write a recursive method
+ * public static boolean isPalindrome(String str)
+ * that returns true if str is a palindrome, that is, a word that is the same when reversed.
+ * Examples of palin drome are “deed”, “rotor”, or “aibohphobia”. Hint: A word is a palindrome if the first and last
+ * letters match and the remainder is also a palindrome.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c5;
+
+/**
+ * Main class.
+ */
+public class BigJavaP5_16 {
+}
