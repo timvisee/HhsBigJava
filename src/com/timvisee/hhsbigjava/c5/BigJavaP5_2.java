e35f2c0f1ac05043352364a0f503f7cbb9f3d551
timvisee <timvisee@gmail.com>
2016-02-11 10:45:14 +0100

    Created the BigJava P5.2 challenge class

---
+++ src/com/timvisee/hhsbigjava/c5/BigJavaP5_2.java
@@ -0,0 +1,28 @@
+/**
+ * BigJava P5.2 challenge:
+ * Write the following methods and provide a program to test them.
+ * a. boolean allTheSame(double x, double y, double z), returning true if the arguments are all the same.
+ * b. boolean allDifferent(double x, double y, double z), returning true if the arguments are all different.
+ * c. boolean sorted(double x, double y, double z), returning true if the arguments are sorted, with the smallest one
+ *     coming first.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c5;
+
+/**
+ * Main class.
+ */
+public class BigJavaP5_2 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
