62030698d2379f545859f1573f5110da709614f7
timvisee <timvisee@gmail.com>
2016-02-11 09:04:13 +0100

    Created BigJava P4.19 class

---
+++ src/com/timvisee/hhsbigjava/c4/BigJavaP4_19.java
@@ -0,0 +1,27 @@
+/**
+ * BigJava P4.19 challenge:
+ * Write a program that prints a multiplication table, like this:
+ *  1  2  3  4  5 ..
+ *  2  4  6  8 10 ..
+ *  3  6  9 ..
+ *  4  8 12 ..
+ *  5 10 15 ..
+ * .. .. ..
+ */
+
+package com.timvisee.hhsbigjava.c4;
+
+/**
+ * Main class.
+ */
+public class BigJavaP4_19 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
