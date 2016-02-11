f089977c2d3999e5af285afbe312ade5c1ffb574
timvisee <timvisee@gmail.com>
2016-02-11 08:57:10 +0100

    Completed BigJava P4.9 challenge

--- src/com/timvisee/hhsbigjava/c4/BigJavaP4_9.java
+++ src/com/timvisee/hhsbigjava/c4/BigJavaP4_9.java
@@ -1,7 +1,39 @@
+/**
+ * BigJava P4.9 challenge:
+ * Write a program that reads a word and prints the word in reverse. For example, if the user provides the input
+ * "Harry", the program prints yrraH
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
 package com.timvisee.hhsbigjava.c4;
 
+import java.util.Scanner;
+
 /**
- * Created by Tim on 11-2-2016.
+ * Main class.
  */
 public class BigJavaP4_9 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+        // Create a scanner for user input
+        Scanner in = new Scanner(System.in);
+
+        // Ask the user for a word
+        System.out.print("Please enter a world: ");
+        String str = in.nextLine();
+
+        // Use a string buffer to rverse the string
+        StringBuilder buff = new StringBuilder(str);
+        str = buff.reverse().toString();
+
+        // Print the resulting string
+        System.out.println("Reversed string: " + str);
+    }
 }
