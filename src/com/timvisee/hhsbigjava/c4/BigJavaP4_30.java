ce16f2beb6dcbec893db1951c17256a108a29e29
timvisee <timvisee@gmail.com>
2016-02-11 10:07:09 +0100

    Created BigJava P4.30 challenge class

---
+++ src/com/timvisee/hhsbigjava/c4/BigJavaP4_30.java
@@ -0,0 +1,24 @@
+/**
+ * BigJava P4.30 challenge:
+ * Write an application to pre­sell a limited number of cinema tickets. Each buyer can buy as
+ * many as 4 tickets. No more than 100 tickets can be sold. Implement a program called TicketSeller that prompts the
+ * user for the desired number of tickets and then displays the number of remaining tickets. Repeat until all tickets
+ * have been sold, and then display the total number of buyers.
+ */
+
+package com.timvisee.hhsbigjava.c4;
+
+/**
+ * Main class.
+ */
+public class BigJavaP4_30 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
