5e62a0db2a6a274ca2ac1ad3f17e0c60bb75e312
timvisee <timvisee@gmail.com>
2016-02-11 10:57:41 +0100

    Created the BigJava P5.3 challenge class

---
+++ src/com/timvisee/hhsbigjava/c5/BigJavaP5_3.java
@@ -0,0 +1,28 @@
+/**
+ * BigJava P5.3 challenge:
+ * Write the following methods.
+ * a. int firstDigit(int n), returning the first digit of the argument.
+ * b. int lastDigit(int n), returning the last digit of the argument.
+ * c. int digits(int n), returning the number of digits of the argument.
+ * For example, firstDigit(1729) is 1, lastDigit(1729) is 9, and digits(1729) is 4. Provide a program that tests your methods.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c5;
+
+/**
+ * Main class.
+ */
+public class BigJavaP5_3 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
