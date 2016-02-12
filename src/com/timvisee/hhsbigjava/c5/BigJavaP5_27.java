668e197570bd4bd63bd05218601d15deb34928a5
timvisee <timvisee@gmail.com>
2016-02-12 13:27:41 +0100

    Created method to convert a roman number to a decimal

--- src/com/timvisee/hhsbigjava/c5/BigJavaP5_27.java
+++ src/com/timvisee/hhsbigjava/c5/BigJavaP5_27.java
@@ -31,4 +31,83 @@ public class BigJavaP5_27 {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * Convert a roman number representation to a decimal.
+     *
+     * @param romanNumber The roman number as a string.
+     *
+     * @return The decimal number
+     */
+    public static int romanToDecimal(String romanNumber) {
+        // Create a variable for the decimal, and the last number
+        int decimal = 0;
+        int lastNumber = 0;
+
+        // Uppercase the string
+        String romanNumeral = romanNumber.toUpperCase();
+
+        // Loop through all the characters in the string
+        for(int x = romanNumeral.length() - 1; x >= 0 ; x--) {
+            // Get the character
+            char convertToDecimal = romanNumeral.charAt(x);
+
+            // Get the decimal value for this character
+            switch (convertToDecimal) {
+                case 'M':
+                    decimal = processDecimal(1000, lastNumber, decimal);
+                    lastNumber = 1000;
+                    break;
+
+                case 'D':
+                    decimal = processDecimal(500, lastNumber, decimal);
+                    lastNumber = 500;
+                    break;
+
+                case 'C':
+                    decimal = processDecimal(100, lastNumber, decimal);
+                    lastNumber = 100;
+                    break;
+
+                case 'L':
+                    decimal = processDecimal(50, lastNumber, decimal);
+                    lastNumber = 50;
+                    break;
+
+                case 'X':
+                    decimal = processDecimal(10, lastNumber, decimal);
+                    lastNumber = 10;
+                    break;
+
+                case 'V':
+                    decimal = processDecimal(5, lastNumber, decimal);
+                    lastNumber = 5;
+                    break;
+
+                case 'I':
+                    decimal = processDecimal(1, lastNumber, decimal);
+                    lastNumber = 1;
+                    break;
+            }
+        }
+
+        // Return the decimal number
+        return decimal;
+    }
+
+    /**
+     * Process a decimal number.
+     *
+     * @param decimal The decimal number.
+     * @param lastNumber The last number.
+     * @param lastDecimal  The last decimal number.
+     *
+     * @return The result.
+     */
+    public static int processDecimal(int decimal, int lastNumber, int lastDecimal) {
+        if (lastNumber > decimal)
+            return lastDecimal - decimal;
+        else
+            return lastDecimal + decimal;
+    }
 }
