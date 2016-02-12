7066d4aef680f19a5158818af593ba450ee0368c
timvisee <timvisee@gmail.com>
2016-02-12 15:33:33 +0100

    Created the BigJava P6.27 base class

---
+++ src/com/timvisee/hhsbigjava/c6/BigJavaP6_27.java
@@ -0,0 +1,32 @@
+/**
+ * BigJava P6.27 challenge:
+ * Write a method
+ * public static ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b)
+ * that merges two array lists, alternating elements from both array lists. If one array list is shorter than the other, then alternate as long as you can and then append the
+ * remaining elements from the longer array list. For example, if a is
+ * 1 4 9 16
+ * and b is
+ * 9 7 4 9 11
+ * then merge returns the array list
+ * 1 9 4 7 9 4 16 9 11
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c6;
+
+/**
+ * Main class.
+ */
+public class BigJavaP6_27 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
