4cf276a9c7800699f810a421379f7e13891af2b2
Tim Visée <timvisee@gmail.com>
2016-02-14 02:17:07 +0100

    Created base of the BigJava P6.33 challenge class

---
+++ src/com/timvisee/hhsbigjava/c6/BigJavaP6_33.java
@@ -0,0 +1,41 @@
+/**
+ * BigJava P6.33 challenge:
+ * You are given a two-dimensional array of values that give the height of a terrain at different points in a square.
+ * Write a method
+ * public static void floodMap(double[][] heights, double waterLevel)
+ * that prints out a flood map, showing which of the points in the terrain would be flooded if the water level was
+ * the given value. In the flood map, print a * for each flooded point and a space for each point that is not flooded.
+ * Here is a sample map:
+ *   * * * *         * *
+ *   * * * * *     * * *
+ *   * * * *         * *
+ *   * * *         * * *
+ *   * * * *     * * * *
+ *   * * * * * * * * * *
+ *   * *     * * *
+ *   *       * * * *   *
+ *                   * *
+ *                 * * *
+ * Then write a program that reads one hundred terrain height values and shows how the terrain gets flooded when the
+ * water level increases in ten steps from the lowest point in the terrain to the highest.
+ *
+ * @author Tim Visee
+ * @website www.timvisee.com
+ */
+
+package com.timvisee.hhsbigjava.c6;
+
+/**
+ * Main class.
+ */
+public class BigJavaP6_33 {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Start arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
